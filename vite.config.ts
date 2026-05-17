19563187765d530a574a0e1d374b119053eee400
lsstreetmma-design <lsstreetmma@gmail.com>
2026-05-17 14:11:18 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-const githubPagesBase = "/street-ready-mind/";
+const githubPagesBase = "/";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
